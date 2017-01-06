da57499c29b86203aba732e1c755e17b9997d0e4
Priyanka <priyanka.rupali@gmail.com>
2017-01-06 19:21:16 +0530

    First Comit

---
+++ Child.java
@@ -0,0 +1,29 @@
+package assignment.acadgild42;
+
+public class Child extends Parent{
+	public void method1() {
+		System.out.println("Child's method1()");
+		
+	}
+	
+
+	public static void main(String args[]) {
+		Parent p = new Child();
+		
+		p.method2();
+	}
+	
+	
+}
+
+class Parent  {
+
+	private void method1() {
+		System.out.println("Parent's method1()");
+	}
+
+	public void method2() {
+		System.out.println("Parent's method2()");
+		method1();
+	}
+}
